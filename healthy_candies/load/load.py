7903569b9c994ebfe4ae2e9996bc6ba4082553ed
Florent Chehab <florent.chehab@gmail.com>
2018-11-18 09:16:30 +0100

    Custom load of only specific columns added

--- healthy_candies/load/load.py
+++ healthy_candies/load/load.py
@@ -2,10 +2,17 @@ from os import path
 import pandas as pd
 from .schema import SCHEMA
 from healthy_candies.path import DATA_FOLDER
+from typing import List
 
 
-def load_data() -> pd.DataFrame:
+def load_data(usecols: List[str] = None) -> pd.DataFrame:
+    """
+        Load the full CSV from openfoodfacts and returns it as a dataframe.
+
+        If `usecols` is specified, then only the specified columns are loaded and returned.
+    """
     return pd.read_csv(
         path.join(DATA_FOLDER, "en.openfoodfacts.org.products.csv"),
         sep="\t",
-        dtype=SCHEMA)
+        dtype=SCHEMA,
+        usecols=usecols)
