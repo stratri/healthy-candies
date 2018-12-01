d4f1039ddc976460b728ec9af3ac312c1919cbfd
Florent Chehab <florent.chehab@gmail.com>
2018-12-01 14:37:53 +0100

    scrap images script added

---
+++ healthy_candies/scrap_images.py
@@ -0,0 +1,32 @@
+"""
+This script is intended to be used to scrapp all product images available on Open Food Facts.
+
+Images are stored in [project]/data/images/
+"""
+
+import urllib
+from os.path import join, exists
+from os import mkdir
+
+import numpy as np
+import pandas as pd
+
+from healthy_candies.load import load_data
+from healthy_candies.load.schema import SCHEMA
+from healthy_candies.path import DATA_FOLDER
+
+IMAGES_FOLDER = join(DATA_FOLDER, 'images')
+if not exists(IMAGES_FOLDER):
+    mkdir(IMAGES_FOLDER)
+
+cols = ['code', 'image_small_url']
+df = load_data(cols, False)
+df = df.fillna('')
+interesting = df[df.image_small_url != '']
+
+for i, r in interesting.iterrows():
+    code = r.code
+    image_url = r.image_small_url
+    ext = image_url.split('.')[-1]
+    destination = join(IMAGES_FOLDER, code + '.' + ext)
+    urllib.request.urlretrieve(image_url, destination)
