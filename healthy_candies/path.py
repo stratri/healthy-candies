e000971012f4bdc60abc5fb22d0725e14ef49a02
Florent Chehab <florent.chehab@gmail.com>
2018-12-14 12:56:48 +0100

    add export path

--- healthy_candies/path.py
+++ healthy_candies/path.py
@@ -5,6 +5,7 @@ ROOT_PROJECT_FOLDER = path.realpath(path.join(file_dir + '/../'))
 
 DATA_FOLDER = path.join(ROOT_PROJECT_FOLDER + '/data/')
 STATIC_DATA_FOLDER = path.join(ROOT_PROJECT_FOLDER + '/static_data/')
+EXPORT_FOLDER = path.join(ROOT_PROJECT_FOLDER + '/export/')
 
 MAIN_DATASET_FILE = path.join(DATA_FOLDER, "en.openfoodfacts.org.products.csv")
 CATEGORIES_OFF_FILE = path.join(STATIC_DATA_FOLDER, 'categoriesOFF.json')
--- init_project.py
+++ init_project.py
@@ -1,4 +1,4 @@
-from healthy_candies.path import DATA_FOLDER, MAIN_DATASET_FILE, WIKIDATA_CAT_TREE_CSV, OFF_CAT_CSV
+from healthy_candies.path import DATA_FOLDER, EXPORT_FOLDER, MAIN_DATASET_FILE, WIKIDATA_CAT_TREE_CSV, OFF_CAT_CSV
 from os.path import exists, join
 import os
 from subprocess import call
@@ -9,6 +9,13 @@ from healthy_candies.init_scipts.cat_off import compute_and_store_off_cat
 if not exists(join(DATA_FOLDER)):
     print("Creating data folder")
     os.makedirs(DATA_FOLDER)
+else:
+    print("Found data folder")
+
+# Setting up the data folder
+if not exists(join(EXPORT_FOLDER)):
+    print("Creating export folder")
+    os.makedirs(EXPORT_FOLDER)
 else:
     print("Found data folder")
 
